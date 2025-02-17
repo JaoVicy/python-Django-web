5b92c9cf62940c138a70fed5e9caa7c5f08173a4
JaoVicy <filipimjoa@gmail.com>
2025-02-17 17:07:13 -0300

    Add post on git admin

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -3,3 +3,12 @@ from django.contrib import admin
 # Register your models here.
 from .models import Post
 from .models import UserProfile
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "slug", "status", "created_on")
+    list_filter = ("status",)
+    search_fields = ("title", "contents")
+    prepopulated_fields = {"slug": ("title",)}
+
+
+admin.site.register(Post, PostAdmin)
