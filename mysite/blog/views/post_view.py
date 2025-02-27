db4d73aa354dbf0bca96bb53a86e1ef74d1b6848
JaoVicy <filipimjoa@gmail.com>
2025-02-27 20:55:42 -0300

    Add order_by and erase order.by

--- mysite/blog/views/post_view.py
+++ mysite/blog/views/post_view.py
@@ -13,7 +13,7 @@ class PostView(generic.View):
 def post_detail(request, slug):
     template_name = "post_detail.html"
     post = get_object_or_404(Post, slug=slug)
-    comments = post.comments.filter(active=True).order.by("-created_on")
+    comments = post.comments.filter(active=True).order_by("-created_on")
     new_comment = None
 
     if request.method == "POST":
